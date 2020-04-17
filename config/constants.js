ff4bfb0adf2a1f1eaeb13bba27ae44c7a6d7d1f2
Carrie Pascale <cpascale43@gmail.com>
2020-04-17 17:06:02 -0400

    chore: add firebase secrets

---
+++ config/constants.js
@@ -0,0 +1,7 @@
+export const FIREBASE_API_KEY = 'AIzaSyB5cx325SVpDRjPQ4FIdDS4jB6pz8xznYU';
+export const FIREBASE_AUTH_DOMAIN = 'plus-one-274519.firebaseapp.com';
+export const FIREBASE_DATABASE_URL = 'plus-one-274519.firebaseio.com';
+export const FIREBASE_PROJECT_ID = 'plus-one-274519';
+export const FIREBASE_STORAGE_BUCKET = 'plus-one-274519.appspot.com';
+export const FIREBASE_APP_ID = '1:378641901121:web:f26e68fde6fb4fb0bc08c0';
+export const FIREBASE_MEASUREMENT_ID = 'G-Q8PSK511Q0';
---
+++ config/firebase.js
@@ -0,0 +1,21 @@
+
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from 'firebase';
+import * as c from './constants';
+// Add the Firebase services that you want to use
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+// Initialize Firebase
+export const config = {
+  apiKey: c.FIREBASE_API_KEY,
+  authDomain: c.FIREBASE_AUTH_DOMAIN,
+  databaseURL: c.FIREBASE_DATABASE_URL,
+  projectId: c.FIREBASE_PROJECT_ID,
+  storageBucket: c.FIREBASE_STORAGE_BUCKET,
+  measurementId: c.FIREBASE_MEASUREMENT_ID
+};
+firebase.initializeApp(config);
+export const db = firebase.firestore();
+export default firebase;
